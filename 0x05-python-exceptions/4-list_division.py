8d7d7cf3d0e11ed910de5d01bf3cfab826fa2299
netia-melvin <melvinatemba18@gmail.com>
2023-06-26 18:38:33 -0700

    Python Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
+# 4-list_division.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists element by element.
+
+    Args:
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
+        list_length (int): The number of elements to divide.
+
+    Returns:
+        A new list of length list_length containing all the divisions.
+    """
     new_list = []
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
-            result = my_list_1[i] / my_list_2[i]
-        except (ValueError, TypeError):
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
             print("wrong type")
-            result = 0
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            result = 0
+            div = 0
         except IndexError:
             print("out of range")
-            result = 0
+            div = 0
         finally:
-            new_list.append(result)
-    return new_list
-
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
-
-print("--")
-
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+            new_list.append(div)
+    return (new_list)
