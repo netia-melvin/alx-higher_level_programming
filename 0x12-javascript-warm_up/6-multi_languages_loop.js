f994d18f02fef88881c4108d3781858f1df7a2a1
netia-melvin <melvinatemba18@gmail.com>
2024-04-09 01:02:29 -0700

    6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (i < arr.length) {
-  console.log(arr[i]);
-  i++;
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lines) {
+  console.log(line);
 }
