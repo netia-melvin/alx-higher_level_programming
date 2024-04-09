c993e577ef7a290e802ca0c6d8452d8d1bc9b48e
netia-melvin <melvinatemba18@gmail.com>
2024-04-08 21:59:55 -0700

    corrections

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Check if at least two arguments are passed
 if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
-    console.log(`${process.argv[2]} is ${process.argv[3]}`);
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
 } else {
-    console.log("Insufficient arguments. Please provide two arguments.");
+  console.log('Insufficient arguments. Please provide two arguments.');
 }
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const num = parseInt(process.argv[2]);
 
 // Check if the conversion resulted in a valid integer
 if (num !== NaN) {
-    console.log(`My number: ${num}`);
+  console.log(`My number: ${num}`);
 } else {
-    console.log("Not a number");
+  console.log('Not a number');
 }
